1c725c4e7d177a0268a781e1298553dd9b3e567d
yamaki <yamaki@yamakiMacBook-Pro.local>
2021-05-12 17:39:15 +0900

    2021/05/15/17:39

--- common/js/animsition.js
+++ common/js/animsition.js
@@ -15,16 +15,16 @@
       options = $.extend({
         inClass               :   'fade-in',
         outClass              :   'fade-out',
-        inDuration            :    1500,
-        outDuration           :    800,
-        linkElement           :   '.animsition-link',
+        inDuration            :    800,
+        outDuration           :    400,
+        linkElement           :   'animsition-link',
         // e.g. linkElement   :   'a:not([target="_blank"]):not([href^=#])'
         loading               :    true,
         loadingParentElement  :   'body', //animsition wrapper element
         loadingClass          :   'animsition-loading',
         loadingInner          :   '', // e.g '<img src="loading.svg" />'
         timeout               :   false,
-        timeoutCountdown      :  5000,
+        timeoutCountdown      :   200,
         onLoadEvent           :   true,
         browser               : [ 'animation-duration', '-webkit-animation-duration'],
         // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
